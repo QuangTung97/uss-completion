1cdc51bcca92bb3b3c1c4606cffaa4cec040c425
QuangTung97 <quangtung29121997@gmail.com>
2026-01-30 10:37:55 +0700

    Add builtin for cat

--- builtin_complete.sh
+++ builtin_complete.sh
@@ -5,7 +5,11 @@ _get_completion_func() {
   echo $function_name
 }
 
-COMPLETION_CD_FUNC=$(_get_completion_func cd)
+# -----------------------------------
+# uss completion wrapper for cd
+# -----------------------------------
+
+COMPLETION_USS_CD_FUNC=$(_get_completion_func cd)
 
 _completion_uss_cd() {
   local word_len=${#COMP_WORDS[@]}
@@ -17,10 +21,35 @@ _completion_uss_cd() {
     COMPREPLY=($(GO_FLAGS_COMPLETE_URI=1 uss "$last_word"))
   else
     # call the underlying completion
-    $COMPLETION_CD_FUNC
+    $COMPLETION_USS_CD_FUNC
   fi
 }
 
-if [[ "$COMPLETION_CD_FUNC" != "_completion_uss_cd" ]]; then
+if [[ "$COMPLETION_USS_CD_FUNC" != "_completion_uss_cd" ]]; then
   complete -o nospace -F _completion_uss_cd cd
+fi
+
+# -----------------------------------
+# uss completion wrapper for cat
+# -----------------------------------
+
+COMPLETION_USS_CAT_FUNC=$(_get_completion_func cat)
+
+_completion_uss_cat() {
+  local word_len=${#COMP_WORDS[@]}
+  local last_word="${COMP_WORDS[$word_len - 1]}"
+  local last_word_trim=${last_word#"\""}
+
+  if [[ "$last_word_trim" == "uss"* ]]; then
+    local IFS=$'\n'
+    compopt -o nospace
+    COMPREPLY=($(GO_FLAGS_COMPLETE_URI=1 uss "$last_word"))
+  else
+    # call the underlying completion
+    $COMPLETION_USS_CAT_FUNC
+  fi
+}
+
+if [[ "$COMPLETION_USS_CAT_FUNC" != "_completion_uss_cat" ]]; then
+  complete -F _completion_uss_cat cat
 fi
